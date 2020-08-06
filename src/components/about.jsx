58fc9bb197fbc84499c4ae7b2e61fc6c30245bfe
Meet Patel <mpatel.patel180796@gmail.com>
2020-08-06 03:50:26 -0400

    added mail popup-copy

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { Grid, Avatar, makeStyles, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Grid,
+  Avatar,
+  makeStyles,
+  Typography,
+  Button,
+  Popover,
+} from "@material-ui/core";
 import { GitHub, LinkedIn, Mail, Twitter, Instagram } from "@material-ui/icons";
 import meet from "../images/meet.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    marginTop: theme.spacing(10),
+    marginTop: theme.spacing(12),
 
     "& > *": {
-      marginTop: theme.spacing(5),
+      marginTop: theme.spacing(3),
     },
+  },
+  popover: {
+    pointerEvents: "none",
+  },
+  paper: {
+    padding: theme.spacing(1),
+    backgroundColor: "transparent",
   },
   image: {
     width: theme.spacing(28),
@@ -31,6 +45,19 @@ const useStyles = makeStyles((theme) => ({
 
 const About = () => {
   const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
   return (
     <Grid
       item
@@ -52,18 +79,77 @@ const About = () => {
         </Typography>
       </Grid>
       <Grid item>
-        <a href="https://www.github.com/meet1807">
+        <Button
+          variant="outlined"
+          color="inherit"
+          href="#resume"
+          className={classes.text}
+        >
+          Resume
+        </Button>
+      </Grid>
+      <Grid item>
+        <a
+          href="https://www.github.com/meet1807"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <GitHub className={classes.icon} />
         </a>
-        <a href="https://www.linkedin.com/in/meet1807/">
+        <a
+          href="https://www.linkedin.com/in/meet1807/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <LinkedIn className={classes.icon} />
         </a>
 
-        <Mail className={classes.icon} />
-        <a href="https://twitter.com/meet_mist">
+        <Mail
+          className={classes.icon}
+          aria-owns={open ? "mouse-over-popover" : undefined}
+          aria-haspopup="true"
+          onMouseEnter={handlePopoverOpen}
+          onMouseLeave={handlePopoverClose}
+          onClick={() => {
+            navigator.clipboard.writeText("meetp6767@gmail.com");
+          }}
+        />
+        <Popover
+          id="mouse-over-popover"
+          className={classes.popover}
+          classes={{
+            paper: classes.paper,
+          }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus
+        >
+          <Typography variant="h6" className={classes.text}>
+            meetp6767@gmail.com
+          </Typography>
+        </Popover>
+
+        <a
+          href="https://twitter.com/meet_mist"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Twitter className={classes.icon} />
         </a>
-        <a href="https://www.instagram.com/_mit777/">
+        <a
+          href="https://www.instagram.com/_mit777/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Instagram className={classes.icon} />
         </a>
       </Grid>
--- src/components/common/cards.jsx
+++ src/components/common/cards.jsx
@@ -1,23 +1,31 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+
 import {
+  makeStyles,
   Card,
   CardMedia,
   CardContent,
   CardActionArea,
   Typography,
   Box,
+  Paper,
+  CardActions,
+  IconButton,
 } from "@material-ui/core";
+
+import ShareIcon from "@material-ui/icons/Share";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: "grid",
     maxWidth: 345,
     margin: theme.spacing(2),
     backgroundColor: "#282c34",
   },
   media: {
     height: 0,
-    objectFit: "contain",
+    objectFit: "cover",
     paddingTop: "56.25%", // 16:9
   },
   title: {
@@ -34,7 +42,28 @@ const useStyles = makeStyles((theme) => ({
   icon: {
     color: "#98a6ad",
     fontSize: theme.spacing(3),
-    marginLeft: theme.spacing(2),
+  },
+
+  paper: {
+    maxHeight: 300,
+    margin: 0,
+    overflow: "auto",
+    backgroundColor: "#282c34",
+    "&::-webkit-scrollbar": {
+      width: "0.4em",
+    },
+    "&::-webkit-scrollbar-track": {
+      boxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+      webkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(0,0,0,.1)",
+      outline: "1px solid slategrey",
+    },
+    body: {
+      alignSelf: "end",
+      textAlign: "center",
+    },
   },
 }));
 
@@ -49,30 +78,46 @@ const Cards = ({ projects }) => {
       elevation={5}
       data-aos="fade-up"
     >
-      <CardActionArea>
-        <CardMedia
-          key={`media${index}`}
-          className={classes.media}
-          image={require(`../../images/${project.image}`)}
-          title={project.title}
-        />
-      </CardActionArea>
-      <Box height="50%">
-        <CardContent>
-          <Box py={2}>
-            <Typography key={`title${index}`} className={classes.title}>
-              {project.title}
-            </Typography>
-          </Box>
+      <Box variant="div" style={{ display: "grid" }}>
+        <div className={classes.body}>
+          <CardActionArea>
+            <CardMedia
+              key={`media${index}`}
+              className={classes.media}
+              image={require(`../../images/${project.image}`)}
+              title={project.title}
+            />
+          </CardActionArea>
 
-          <Typography
-            key={`content${index}`}
-            className={classes.content}
-            align="left"
-          >
-            {project.description}
-          </Typography>
-        </CardContent>
+          <CardContent>
+            <Box py={2}>
+              <Typography key={`title${index}`} className={classes.title}>
+                {project.title}
+              </Typography>
+            </Box>
+
+            <Paper className={classes.paper} elevation={0}>
+              <Typography
+                key={`content${index}`}
+                className={classes.content}
+                align="left"
+              >
+                {project.description}
+              </Typography>
+            </Paper>
+          </CardContent>
+        </div>
+        <CardActions
+          disableSpacing
+          style={{ display: "flex", alignSelf: "end" }}
+        >
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon className={classes.icon} />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon className={classes.icon} />
+          </IconButton>
+        </CardActions>
       </Box>
     </Card>
   ));
